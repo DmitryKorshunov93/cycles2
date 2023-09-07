1aefd1d5153dbab27dea82a736409399ae086692
De93m <DmitryKorshunov93@gmail.com>
2023-09-07 13:41:54 +0300

    homework

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,103 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.println("Задание №1");
+        {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            int month = 15_000;
+            int total = 0;
+            int i = 0;
+            while (total <= 2_459_000) {
+                i++;
+                total = total + total / 100;
+                total = total + month;
+                System.out.println("Месяц " + i + " сумма накоплений равна " + total + " рублей");
+            }
+        }
+
+        System.out.println("Задание №2");
+        int i = 0;
+        while (i < 10) {
+            i++;
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+
+        for (i = 10; i > 0; i--) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+
+        System.out.println("Задание №3");
+
+        int countryPopulation = 12_000_000;
+        int fertility = countryPopulation / 1000 * 17;
+        int mortality = countryPopulation / 1000 * 8;
+        int populationGrowth = fertility - mortality;
+        for (i = 1; i <= 10; i++) {
+            countryPopulation = countryPopulation + populationGrowth;
+            System.out.println("Год " + i + ", численность населения составляет " + countryPopulation);
+        }
+
+
+        { System.out.println("Задание №4");
+
+            int initialDepositAmount = 15_000;
+            int total = 0;
+            int m = 0;
+            while (total <= 12_000_000) {
+                m++;
+                total = total + total / 100 * 7;
+                total = total + initialDepositAmount;
+                System.out.println("Месяц " + m + " сумма накоплений равна " + total + " рублей");}}
+
+        {
+
+        System.out.println("Задание №5");
+
+        int initialDepositAmount = 15_000;
+        int total = 0;
+        int m = 0;
+        while (total <= 12_000_000) {
+            m++;
+            total = total + total / 100 * 7;
+            total = total + initialDepositAmount;
+            if ( m % 6 == 0){
+            System.out.println("Месяц " + m + " сумма накоплений равна " + total + " рублей");}}}
+
+        System.out.println("Задание №6");
+
+        int initialDepositAmount = 15_000;
+        int total = 0;
+        int yers = 9 * 12;
+        for (i = 0; i <= yers; i ++) {
+            total = total + total / 100 * 7;
+            total = total + initialDepositAmount;
+            if ( i % 6 == 0){
+                System.out.println("Месяц " + i + " сумма накоплений равна " + total + " рублей");}}
+
+        System.out.println("Задание №7");
+
+        int friday = 5;
+        for (i = friday; i <= 31; i+=7){
+            System.out.println("Сегодня пятница, " + i + "-е число. Необходимо подготовить отчет");}
+
+        System.out.println("Задание №8");
+
+        int years = 2023;
+        int years200 = years - 200;
+        int years100 = years + 200;
+
+        for (i = 0; i <=2123; i+=79) {
+            if (i >= years200 && i <= years100)
+            System.out.println(i);
         }
     }
+
 }
+
+
+
